44343cd8d0d0100dee6d0f5d82213ac28790bd6b
Casey McCoy <caseymccoy03081998@gmail.com>
2020-04-19 20:08:02 -0400

    Added queries to setup default tables.

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const cors = require('cors');
 //const path = require('path');
 const apiRoute = require('./api.route');
 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234qwer',
+    database: 'cosc484'
+})
+connection.connect((err) => {
+    if (err) throw err
+    console.log("[mysql] Connected successfully")
+})
+
 // Express
 const app = express();
 
@@ -30,3 +42,29 @@ const server = app.listen(port, () => {
     console.log('Connected to port ' + port);
 });
 
+// SQL
+
+connection.query('CREATE TABLE IF NOT EXISTS user(id INT SIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,'
+                                    + 'name VARCHAR(50) NOT NULL,'
+                                    + 'password TEXT NOT NULL,'
+                                    + 'profile_picture TEXT,'
+                                    + 'followed_users JSON, '
+                                    + 'average_rating JSON,'
+                                    + 'favorites JSON, '
+                                    + 'biometric_data JSON)',
+                                    function(err, result){
+                                        if(err) throw err
+                                        console.log("[mysql] User table query successful")
+                                    })
+
+connection.query('CREATE TABLE IF NOT EXISTS workouts(id INT SIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,'
+                                    + 'title VARCHAR(50) NOT NULL,'
+                                    + 'description TEXT NOT NULL,'
+                                    + 'total_favorites INT SIGNED,'
+                                    + 'total_views BIGINT SIGNED, '
+                                    + 'workplans JSON,'
+                                    + 'favorites JSON)',
+                                    function(err, result){
+                                        if(err) throw err
+                                        console.log("[mysql] Workout table query successful")
+                                    })
