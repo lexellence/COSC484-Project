517a9b02f3ebd265e81867abbc7332941cab52cc
David Leksen <dleksen@protonmail.com>
2020-04-21 04:34:01 -0400

    Added a couple endpoint stubs, commented out some endpoint stubs that are totally empty or not sure about.

--- api.route.js
+++ api.route.js
@@ -3,99 +3,117 @@ const router = express.Router();
 
 // Test API
 router.route('/how-is-today').get((req, res, next) => {
-    res.send('great');
+	res.send('great');
 });
 
 // Fake data for stubs
 const userProfile1 = {
-    id: 123,
-    firstname: 'first',
-    lastname: 'last',
-    email: 'email1@mail.com',
-    profilePic: 'picFilename.png',
-    following: [124],
-    workouts: [987],
-    favorites: [987, 988],
-    workoutActivity: 'biometric data?'
+	id: 123,
+	firstname: 'first',
+	lastname: 'last',
+	email: 'email1@mail.com',
+	profilePic: 'picFilename.png',
+	following: [124],
+	workouts: [987],
+	favorites: [987, 988],
+	workoutActivity: 'biometric data?'
 };
 const userProfile2 = {
-    id: 124,
-    firstname: 'first2',
-    lastname: 'last2',
-    email: 'email2@mail.com',
-    profilePic: 'picFilename2.png',
-    following: [123],
-    workouts: [988],
-    favorites: [987, 988],
-    workoutActivity: 'biometric data?'
+	id: 124,
+	firstname: 'first2',
+	lastname: 'last2',
+	email: 'email2@mail.com',
+	profilePic: 'picFilename2.png',
+	following: [123],
+	workouts: [988],
+	favorites: [987, 988],
+	workoutActivity: 'biometric data?'
 };
 const userProfiles = [userProfile1, userProfile2];
 const workout1 = {
-    id: 987,
-    userID: 123,
-    title: "Power Lifting",
-    description: "Achieve max gainz",
-    numFavorites: 245,
-    numViews: 576,
-    rating: 4.5,
-    hashtags: ['power lifting', 'max gainz'],
-    videoLink: "https://youtube.com/example"
+	id: 987,
+	userID: 123,
+	title: "Power Lifting",
+	description: "Achieve max gainz",
+	numFavorites: 245,
+	numViews: 576,
+	rating: 4.5,
+	hashtags: ['power lifting', 'max gainz'],
+	videoLink: "https://youtube.com/example"
 };
 const workout2 = {
-    id: 988,
-    userID: 123,
-    title: "Workout2",
-    description: "Description2",
-    numFavorites: 12,
-    numViews: 33,
-    rating: 4.2,
-    hashtags: ['hashtag1', 'hashtag2'],
-    videoLink: "https://youtube.com/example2"
+	id: 988,
+	userID: 123,
+	title: "Workout2",
+	description: "Description2",
+	numFavorites: 12,
+	numViews: 33,
+	rating: 4.2,
+	hashtags: ['hashtag1', 'hashtag2'],
+	videoLink: "https://youtube.com/example2"
 };
 const workouts = [workout1, workout2];
 
 
 // User list API
-// router.route('/create-user').post((req, res, next) => {
+// router.route('/create-user').post((req, res) => {
 //     res.sendStatus(201); // 201 = created
 // });
-router.route('/get-user-profile-list').get((req, res, next) => {
-    res.status(200).json(userProfiles);   // 200 = OK
+router.route('/get-user-profile-list').get((req, res) => {
+	res.status(200).json(userProfiles);   // 200 = OK
 });
-router.route('/get-user-profile/:id').get((req, res, next) => {
-    let foundProfile = userProfiles.find((profile, i, array) => {
-        return profile.id === req.params.id;
-    });
-    if (foundProfile)
-        res.status(200).json(foundProfile);   // 200 = OK
-    else
-        res.sendStatus(404);   // 404 = not found
+router.route('/get-user-profile/:id').get((req, res) => {
+	let foundProfile = userProfiles.find((profile, i, array) => {
+		return profile.id === req.params.id;
+	});
+	if (foundProfile)
+		res.status(200).json(foundProfile);   // 200 = OK
+	else
+		res.sendStatus(404);   // 404 = not found
 });
-// router.route('/update-user/:id').put((req, res, next) => {
+// router.route('/update-user/:id').put((req, res) => {
 //     res.sendStatus(200);
 // });
-// router.route('/delete-user/:id').delete((req, res, next) => {
+// router.route('/delete-user/:id').delete((req, res) => {
 //     res.sendStatus(200);
 // });
 
-router.route('/get-trending/:num').get((req, res, next) => {
-    if (req.params.num < 1)
-        res.sendStatus(400);    // 400 = bad request
+router.route('/get-trending/:num').get((req, res) => {
+	if (req.params.num < 1)
+		res.sendStatus(400);    // 400 = bad request
 
-    if (req.params.num >= workouts.length)
-        res.status(200).json(workouts);   // 200 = OK
-    else
-        res.status(200).json(workouts.slice(0, req.params.num));   // 200 = OK
-});
-router.route('/get-favorites/:id').get((req, res, next) => {
-    // if (req.params.id < 0)
-    //     res.sendStatus(400);    // 400 = bad request
-
-    if (req.params.num >= workouts.length)
-        res.status(200).json(workouts);   // 200 = OK
-    else
-        res.status(200).json(workouts.slice(0, req.params.num));   // 200 = OK
+	if (req.params.num >= workouts.length)
+		res.status(200).json(workouts);   // 200 = OK
+	else
+		res.status(200).json(workouts.slice(0, req.params.num));   // 200 = OK
 });
 
+//+------------------------\----------------------------------
+//|	    Get Favorites      |
+//\------------------------/----------------------------------
+//	Responds with array of newest workouts from followed users
+//------------------------------------------------------------
+router.route('/get-favorites/:userid').get((req, res) => {
+	// if (req.params.id < 0)
+	//     res.sendStatus(400);    // 400 = bad request
+
+	if (req.params.num >= workouts.length)
+		res.status(200).json(workouts);   // 200 = OK
+	else
+		res.status(200).json(workouts.slice(0, req.params.num));   // 200 = OK
+});
+
+//+------------------------\----------------------------------
+//|	       Get Feed        |
+//\------------------------/----------------------------------
+//	Responds with array of 'max' newest workouts from followed users
+//------------------------------------------------------------
+router.route('/get-feed/:userid/:max').get((req, res) => {
+	// STUB
+	if (req.params.max >= workouts.length)
+		res.status(200).json(workouts);   // 200 = OK
+	else
+		res.status(200).json(workouts.slice(0, req.params.num));   // 200 = OK
+});
 
 module.exports = router;
