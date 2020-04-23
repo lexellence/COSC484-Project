9719792e70efad67ceee6af1a3086a3f67b5ddeb
Haley <hturfl1@students.towson.edu>
2020-04-23 11:44:24 -0400

    lets try this again-html from template.html added to App.js in return method

--- frontend/exer/src/App.js
+++ frontend/exer/src/App.js
@@ -6,8 +6,29 @@ import postList from './posts/postList';
 
 class App extends Component {
   render() {
-  return (
+  return (//from template.html
     <div className="App">
+      
+      <div className="theme-switch-wrapper">
+        <label className="theme-switch" for="checkbox">
+        <input type="checkbox" id="checkbox" />
+        <div className="slider round"></div>
+        </label>
+        <em>Dark Mode</em>
+      </div>
+
+      <div className="headerScript">
+        <script src="DarkMode.js"></script>
+        <script lang="javascript" type="text/javascript" src="header.js"></script>
+      </div>
+    
+      <div className="imgbox">
+        <img className="center-fit" src="homepageGraphic.jpg" alt="EXER Homepage Collage Graphic"/>
+      </div>
+
+      <div className="footerScript">
+        <script lang="javascript" type="text/javascript" src="footer.js"></script>
+      </div>
 
     </div>
   );
