3c3376c82764e0ccce55396b964b86cbd45be6fd
B. Lim <blim.digital@gmail.com>
2020-04-29 21:38:13 -0400

    changes to navbar, beginning total rework of landing page

--- frontend/exer/src/components/navbar.js
+++ frontend/exer/src/components/navbar.js
@@ -1,9 +1,15 @@
 import React from 'react'
+//import logo from '../logo.png';
 
 function navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-        <a className="navbar-brand" href="#">Navbar</a>
+        
+        {/*  
+        <a className="navbar-brand" href="#">
+            <img src={logo} alt="EXER-HOME"/>
+        </a> */}
+
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
@@ -11,24 +17,16 @@ function navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a className="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">Something else here</a>
-                </div>
+                <a className="nav-link text-white" href="#">About</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                <a className="nav-link text-white" href="#">Contact</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link text-white" href="#">Register</a>
             </li>
             </ul>
             <form className="form-inline my-2 my-lg-0">
