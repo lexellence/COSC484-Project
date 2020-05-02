39610c80441e275829eee582be8c17bcebf24982
IsaacTray <44411173+IsaacTray@users.noreply.github.com>
2020-05-01 23:17:30 -0400

    Update postList.js

--- frontend/exer/src/posts/postList.js
+++ frontend/exer/src/posts/postList.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import postData from '../data/posts.json';
 
 class postList extends Component {
-    render(){
-    return (
-        <div>
-            {postData.map(() => {
-                
-            })} 
-        </div>
-    )
-    }//end render()
-}//end component()
+    render() {
+      return <div className="App">
+          <Nav />
+          <section className="App-main">
+            <Post nickname="Chris" avatar= {example1} caption="I wish I could go outside!" image= {example1}/>
+            <Post nickname="Not Chris" avatar= {example2} caption="I wish I was Chris" image= {example2}/>
 
+            {/* more posts */}
+          </section>
+        </div>;
+    }
+  }
 export default postList;
