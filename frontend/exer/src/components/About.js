e5b62e19797363967c82c4a0ac0486593b5a32e3
B. Lim <blim.digital@gmail.com>
2020-05-02 19:03:44 -0400

    added info cards & imports to About.js

--- frontend/exer/src/components/About.js
+++ frontend/exer/src/components/About.js
@@ -1,25 +1,63 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import diet from '../assets/diet.svg';
+import application from '../assets/application.svg';
+import goal from '../assets/goal.svg';
+
 
 function About() {
   return (
     <div>
       <Container className="text-center mt-5 mb-5">
+         <hr></hr>
         <h1>About Us</h1>
         <div className="mt-4">
-          <p>Nullam eget faucibus sem. Vestibulum ante ipsum primis in faucibus orci
-          luctus et ultrices posuere cubilia curae; Aenean dictum commodo ullamcorper. 
-          Pellentesque dignissim urna ut lectus venenatis, et ullamcorper ipsum egestas. 
-          Nullam sollicitudin feugiat est ut molestie. Nulla vitae elit urna. Sed et felis eu 
-          justo suscipit dictum quis a metus. Aenean in pharetra nunc.</p>
-          <p>
-          Duis sed massa in mi dictum euismod. Vestibulum sed risus nibh. In non posuere sapien. 
-          Nulla scelerisque vehicula pharetra. Nam ullamcorper ut arcu vitae dapibus. Etiam rutrum consectetur justo. 
-          Nullam eget augue justo. Quisque dictum id augue eu volutpat. Aliquam quis maximus augue. Donec ante mauris, 
-          sagittis ut ipsum et, placerat condimentum mi. Cras vel mollis velit. Quisque consectetur vehicula imperdiet. 
-          Etiam egestas laoreet ex, eget faucibus quam commodo eu. Pellentesque sed leo ligula. Phasellus sit amet 
-          massa aliquam, malesuada odio id, volutpat mauris.
-          </p>
+          <p className="about-subtitle"> <b>EXER</b> is an interactive fitness application for training circuits, sharing results, and lifestyle tips. Track your fitness and share your progress with friends!</p>
+          <br></br>
+          <CardDeck>
+          <Card>
+            <Card.Img variant="top" src={goal} />
+            <Card.Body>
+              <Card.Title><b>Set Goals</b></Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted"><div>Icons made by <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">iconixar</a></div></small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src={application} />
+            <Card.Body>
+              <Card.Title><b>Learn Freely</b></Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to additional
+                content.{' '}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted"><div>Icons made by <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">iconixar</a></div></small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src={diet} />
+            <Card.Body>
+              <Card.Title><b>Track Progress</b></Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This card has even longer content than the first to
+                show that equal height action.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted"><div>Icons made by <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">iconixar</a></div></small>
+            </Card.Footer>
+          </Card>
+        </CardDeck>
         </div>
       </Container>
     </div>
