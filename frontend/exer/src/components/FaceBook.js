b561e287730a24993330e14d6b875a34048c4b36
Haley <hturfl1@students.towson.edu>
2020-05-04 11:28:41 -0400

    reformatted TermsOfService.js, PrivacyPolicy.js, created social media accts & added links to FaceBook.js, Twitter.js, Instagram.js

--- frontend/exer/src/components/FaceBook.js
+++ frontend/exer/src/components/FaceBook.js
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
 import FaceBookPic from '../assets/facebook.jpg';
+import SocialMeda1 from '../assets/socialmedia.jpg';
+import SocialMedia2 from '../assets/socialmedia2.jpg';
 
 class FaceBook extends Component {
   render() {
@@ -10,17 +12,19 @@ class FaceBook extends Component {
       <div>
         <Container className="text-center mt-5 mb-5">
           <hr></hr>
-          <h1>Our FaceBook</h1>
+          <h1>FaceBook</h1>
           <p className="facebook-subtitle">
-          <img height="200" width="280" src={FaceBookPic}></img>
+                <img height="200" width="280" src={SocialMeda1}></img>
+                <img height="200" width="280" src={FaceBookPic}></img>
+                <img height="200" width="280" src={SocialMedia2}></img>
           <div className="mt-4">
             <p className="facebook-subtitle">
             <CardDeck>
               <Card>
                 <Card.Body>
-                  <Card.Title><b>Follow Us</b></Card.Title>
+                  <Card.Title><b>Keep up to date with our FaceBook page</b></Card.Title>
                   <Card.Text>
-                    ________
+                    <a href="https://www.facebook.com/EXER-111118643923266/">EXER on FaceBook</a>
                   </Card.Text>
                 </Card.Body>
               </Card>
--- frontend/exer/src/components/Twitter.js
+++ frontend/exer/src/components/Twitter.js
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
 import TwitterPic from '../assets/twitter.jpg';
+import SocialMeda1 from '../assets/socialmedia.jpg';
+import SocialMedia2 from '../assets/socialmedia2.jpg';
 
 class Twitter extends Component {
   render() {
@@ -10,17 +12,19 @@ class Twitter extends Component {
       <div>
         <Container className="text-center mt-5 mb-5">
           <hr></hr>
-          <h1>Our Twitter</h1>
+          <h1>Twitter</h1>
           <p className="twitter-subtitle">
-          <img height="200" width="280" src={TwitterPic}></img>
+                <img height="200" width="280" src={SocialMeda1}></img>
+                <img height="200" width="280" src={TwitterPic}></img>
+                <img height="200" width="280" src={SocialMedia2}></img>
           <div className="mt-4">
             <p className="twitter-subtitle">
             <CardDeck>
               <Card>
                 <Card.Body>
-                  <Card.Title><b>Follow Us</b></Card.Title>
+                  <Card.Title><b>Keep to date by following us on Twitter</b></Card.Title>
                   <Card.Text>
-                    ________
+                  <a href="https://twitter.com/EXER83828318">EXER on Twitter</a>
                   </Card.Text>
                 </Card.Body>
               </Card>
