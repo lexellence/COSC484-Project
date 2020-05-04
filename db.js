4151db231d8ce384931c002ba12b83560936f0df
feram18 <feram99ag@gmail.com>
2020-05-03 23:25:58 -0400

    Updated db tables. Removed 'env' file that was not intended to be on the repo.

--- db.js
+++ db.js
@@ -5,36 +5,41 @@ const mysql = require('mysql');
 const connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect((err) => {
-	if (err)
+	if (err) {
+		console.log("DB error", err);
 		throw err;
+	}
 	console.log("[mysql] Connected successfully");
 });
 
-// Make sure SQL tables exist
-connection.query('CREATE TABLE IF NOT EXISTS user(id INT SIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,'
-	+ 'firstname VARCHAR(50) NOT NULL,'
-	+ 'lastname VARCHAR(50) NOT NULL,'
-	+ 'password TEXT NOT NULL,'
-	+ 'profile_picture TEXT,'
-	+ 'followed_users JSON, '
-	+ 'work_plans JSON,'
-	+ 'favorites JSON, '
-	+ 'biometric_data JSON)',
+// Ensure SQL tables exist, create them if not
+connection.query('CREATE TABLE IF NOT EXISTS `user` (`id` int(11) NOT NULL AUTO_INCREMENT, '
+	+ '`firstname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,'
+	+ '`lastname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,'
+	+ '`username` varchar(45) COLLATE utf8_unicode_ci NOT NULL,'
+	+ '`email` varchar(45) COLLATE utf8_unicode_ci NOT NULL,'
+	+ '`password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,'
+	+ '`profile_picture` text COLLATE utf8_unicode_ci,'
+	+ '`followed_users` json DEFAULT NULL,'
+	+ '`posts_id` json DEFAULT NULL,'
+	+ '`fav_posts_id` json DEFAULT NULL,'
+	+ 'PRIMARY KEY (`id`)'
+	+ ') ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;',
 	function (err, result) {
 		if (err) throw err;
-		console.log("[mysql] Users table query successful");
+		console.log("[mysql] User table query successful");
 	});
 
-connection.query('CREATE TABLE IF NOT EXISTS workouts(id INT SIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,'
-	+ 'user_id INT NOT NULL,'
-	+ 'title VARCHAR(50) NOT NULL,'
-	+ 'description TEXT NOT NULL,'
-	+ 'total_favorites INT SIGNED,'
-	+ 'total_views BIGINT SIGNED, '
-	+ 'average_rating JSON,'
-	+ 'link TEXT)',
+connection.query('CREATE TABLE IF NOT EXISTS `posts` (`id` int(11) NOT NULL AUTO_INCREMENT,'
+	+ '`user_id` int(11) NOT NULL,'
+	+ '`title` varchar(50) COLLATE utf8_unicode_ci NOT NULL,'
+	+ '`body` text COLLATE utf8_unicode_ci NOT NULL,'
+	+ '`total_favorites` int(11) DEFAULT NULL,'
+	+ '`comments` json DEFAULT NULL,'
+	+ 'PRIMARY KEY (`id`)'
+	+ ') ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;',
 	function (err, result) {
-		if (err) throw "[mysql] ERROR - " + err;
+		if (err) throw err;
 		console.log("[mysql] Posts table query successful");
 	});
 
