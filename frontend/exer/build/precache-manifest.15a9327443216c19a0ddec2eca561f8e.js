9b3cac4d0ff11e60969b06701cb41f084b1f5cdf
Luis Ramos <feram99ag@gmail.com>
2020-05-05 02:14:33 -0400

    Testing Heroku deployment of front-end

---
+++ frontend/exer/build/precache-manifest.15a9327443216c19a0ddec2eca561f8e.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6549b6e4fee03a842d20aaad5a00ba62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e67e759bfde932bda0d",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "d83cfc8c0fd234bb1366",
+    "url": "/static/css/main.ff542247.chunk.css"
+  },
+  {
+    "revision": "5e67e759bfde932bda0d",
+    "url": "/static/js/2.28080e68.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.28080e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d83cfc8c0fd234bb1366",
+    "url": "/static/js/main.fe6bac8c.chunk.js"
+  },
+  {
+    "revision": "2d110c182046865120c3",
+    "url": "/static/js/runtime-main.de1b796b.js"
+  },
+  {
+    "revision": "f47c425e19fdb36d057a51f90c317304",
+    "url": "/static/media/application.f47c425e.svg"
+  },
+  {
+    "revision": "c41fa3a6461670f3a891d023c82aab6a",
+    "url": "/static/media/connect.c41fa3a6.jpg"
+  },
+  {
+    "revision": "c17cb4e4d8cc61de8534a1f2ecb6b923",
+    "url": "/static/media/connections.c17cb4e4.jpg"
+  },
+  {
+    "revision": "07832bfaa32c68698f93a5d1d93e7024",
+    "url": "/static/media/demo-profile-picture.07832bfa.png"
+  },
+  {
+    "revision": "f4da44a0edbcd5825ebf185f659e0a4a",
+    "url": "/static/media/diet.f4da44a0.svg"
+  },
+  {
+    "revision": "256aae48fe76cec9cbaf79c91d6077cc",
+    "url": "/static/media/facebook.256aae48.jpg"
+  },
+  {
+    "revision": "e3bfa72d894e018b7b02171d3b2bfbe1",
+    "url": "/static/media/goal.e3bfa72d.svg"
+  },
+  {
+    "revision": "33787bb4e27857184a915751767b6298",
+    "url": "/static/media/instagram.33787bb4.jpg"
+  },
+  {
+    "revision": "08eaf7f916468158225af602b4383f38",
+    "url": "/static/media/jelmer-assink-gzeTjGu3b_k-unsplash.08eaf7f9.jpg"
+  },
+  {
+    "revision": "ed79c65af1140820115aa7e667f493a0",
+    "url": "/static/media/logo.ed79c65a.png"
+  },
+  {
+    "revision": "54d2a06c0b7ad364bf3c6a635f9d2f8c",
+    "url": "/static/media/person-holding-barbell-841130.54d2a06c.jpg"
+  },
+  {
+    "revision": "07ceefa1ead97390b1f2cde4e955883c",
+    "url": "/static/media/person-holding-black-dumbbells-1032117.07ceefa1.jpg"
+  },
+  {
+    "revision": "bb1b078d62f01bd8ace14777b4287ac9",
+    "url": "/static/media/privacy.bb1b078d.jpg"
+  },
+  {
+    "revision": "7f29fd47e64594791ecc53a63937fbfb",
+    "url": "/static/media/socialmedia.7f29fd47.jpg"
+  },
+  {
+    "revision": "98cb8d8fdbe9eee2ebdc4a1bc344443f",
+    "url": "/static/media/socialmedia2.98cb8d8f.jpg"
+  },
+  {
+    "revision": "f710891c1b01408f6b244ab9d8b13d5b",
+    "url": "/static/media/support.f710891c.jpg"
+  },
+  {
+    "revision": "1ebaadfe94240a580ec7b6903001f312",
+    "url": "/static/media/team.1ebaadfe.jpg"
+  },
+  {
+    "revision": "ce2a766cff2d2f846ad0ebf0a0a21b41",
+    "url": "/static/media/termsOfService.ce2a766c.jpg"
+  },
+  {
+    "revision": "60816f17451645848d32f1bbd15417b9",
+    "url": "/static/media/twitter.60816f17.jpg"
+  }
+]);
